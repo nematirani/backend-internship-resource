1263913792d918f2b23b01a9c0344ff3e5265ea3
nemat82 <52141207+nemat82@users.noreply.github.com>
2021-09-09 03:27:21 +0430

    solve combine

--- python/kata/combine.py
+++ python/kata/combine.py
@@ -1,8 +1,11 @@
 def combine_together(d1, d2):
-    """ Write a code to combine two dictionary adding values for common keys. """
 
-
+    for i in d2.keys():
+        if i in d1:
+            d2[i] = d2[i] + d1[i]
+          
 d1 = {'a': 100, 'b': 200, 'c':300}
-d2 = {'a': 300, 'b': 200, 'd':400}
+d2 = {'a': 300, 'b': 200, 'c':400}
 print(combine_together(d1, d2))
+print(d2)
 # Expected Result: {'a': 400, 'b': 400, 'd': 400, 'c': 300}
