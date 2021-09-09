c9d6eb67f159c4608f735e65621fbeaa6001b690
nemat82 <52141207+nemat82@users.noreply.github.com>
2021-09-09 16:15:22 +0430

    edit combine.py

--- python/kata/combine.py
+++ python/kata/combine.py
@@ -1,11 +1,14 @@
 def combine_together(d1, d2):
-
     for i in d2.keys():
         if i in d1:
-            d2[i] = d2[i] + d1[i]
-          
-d1 = {'a': 100, 'b': 200, 'c':300}
-d2 = {'a': 300, 'b': 200, 'c':400}
+            d2[i] += d1[i]
+    for b in d1.keys():
+        if b not in d2:
+            d2[b] = d1[b]
+    return d2
+
+
+d1 = {'a': 200, 'b': 200, 'c':300}
+d2 = {'a': 200, 'b': 200, 'd':400}
 print(combine_together(d1, d2))
-print(d2)
 # Expected Result: {'a': 400, 'b': 400, 'd': 400, 'c': 300}
