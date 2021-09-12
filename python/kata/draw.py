6c053aba30dc78fb9df8fbbc76c78459cec4d552
nemat82 <52141207+nemat82@users.noreply.github.com>
2021-09-13 00:58:21 +0430

    solve draw

--- python/kata/draw.py
+++ python/kata/draw.py
@@ -2,6 +2,14 @@ def draw_triangle():
     """ Write a code to construct the following pattern, 
     using a nested for loop. """
 
+    for i in range(5):
+        for b in range(i):
+            print("*", end=' ')
+        print(' ')
+    for c in range(5,0,-1):
+        for d in range(c):
+            print("*", end=' ')
+        print(' ')
 
 
 draw_triangle()
