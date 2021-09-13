2480f53d0f4838216c00c9283689609ae3feaf09
nemat82 <52141207+nemat82@users.noreply.github.com>
2021-09-13 23:56:11 +0430

    solve maximum

--- python/kata/maximum.py
+++ python/kata/maximum.py
@@ -1,5 +1,12 @@
 def max_aggregate(score_list):
     """ Write a code to calculate the maximum aggregate from the list of tuples (pairs). """
+    b = []
+    for i in score_list:
+        b.append(i[1])
+    c = max(b)
+    for d in score_list:
+        if c in d:
+            return d
 
 
 print(max_aggregate([
