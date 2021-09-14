f539cad766eb7e422d6d071afaba604b3a2c5882
nemat82 <52141207+nemat82@users.noreply.github.com>
2021-09-15 00:53:59 +0430

    solve sort

--- python/kata/sort.py
+++ python/kata/sort.py
@@ -1,7 +1,8 @@
 def sort_by_last_element(numbers):
     """ Write a code to get a list, sorted in increasing order by the last element
     in each tuple from a given list of non-empty tuples."""
+    return sorted(numbers, key = lambda x: x[-1])  
 
-
+    
 print(sort_by_last_element([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
 # Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
