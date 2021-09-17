ca69e8890f8b9f469aff06ff4653eddcd4c405e9
nemat82 <52141207+nemat82@users.noreply.github.com>
2021-09-17 22:23:43 +0430

    solve mro

--- python/kata/mro.py
+++ python/kata/mro.py
@@ -36,7 +36,11 @@ class H(G, F, B):
 
 
 
-""" Describe the order of the mro algorithm """
+""" 
+MRO is used primarily to obtain the order in which methods should be inherited in the presence of multiple inheritance. 
+Python 3 uses the C3 linearization algorithm for MRO. 
+L[C] = C + merge of linearization of parents of C and list of parents of C in the order they are inherited from left to right.
+ """
 
 print(H.__mro__)
 # Result: (<class '__main__.H'>, <class '__main__.G'>, <class '__main__.D'>, <class '__main__.F'>, <class '__main__.B'>, <class '__main__.E'>, <class '__main__.C'>, <class '__main__.A'>, <class 'object'>)
