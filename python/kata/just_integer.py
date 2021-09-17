1d896b92f4534065f115b31f4beea85d1f8f336c
nemat82 <52141207+nemat82@users.noreply.github.com>
2021-09-17 21:53:47 +0430

    solve just_integer

--- python/kata/just_integer.py
+++ python/kata/just_integer.py
@@ -1,8 +1,13 @@
 def just_integer(collection):
     """ Write a code to filter collection to get integer values
     using Python filter and local function. """
-
+    collection = [x for x in collection if isinstance(x,int) and not isinstance(x,bool)]
+    return collection
 
 c = ['Kourosh', 1, None, True, [], 2, 3, ()]
 
+
+
 assert just_integer(c) == [1, 2, 3]
+
+
